7d36f406b7193009d2779b5028ccaf83412e75b5
zuparomana <adam.romanowski93@gmail.com>
2018-08-28 10:45:33 +0200

    Implement main method

---
+++ src/main/java/guestBook/App.java
@@ -0,0 +1,28 @@
+package guestBook;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        HttpServer httpServer = null;
+        try {
+            httpServer = HttpServer.create(new InetSocketAddress(8000), 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        httpServer.createContext("/index", new GuestBook());
+
+        httpServer.setExecutor(null);
+
+        httpServer.start();
+    }
+}
